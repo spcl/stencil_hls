56b0ea8dc3642e7bf96acf3b6e51a711107c9fb9
Johannes de Fine Licht <definelj@student.ethz.ch>
2017-03-12 11:02:58 +0000

    Recognize failed placement and crashes

--- scripts/scan_configurations.py
+++ scripts/scan_configurations.py
@@ -148,7 +148,7 @@ def extract_result_build(conf):
     conf.consumption = Consumption(conf, "no_build", None, None, None, None,
                                    None, None)
     return
-  status = check_build_status(kernelFolder)
+  status = check_build_status(buildFolder)
   reportPath = os.path.join(
       implFolder, "xcl_design_wrapper_utilization_placed.rpt")
   if not os.path.isfile(reportPath):
@@ -191,12 +191,26 @@ def extract_result_build(conf):
   conf.consumption = Consumption(
       conf, status, luts, ff, dsp, bram, power, clock)
 
-def check_build_status(kernelFolder):
+def check_build_status(buildFolder):
+  kernelFolder = os.path.join(
+      buildFolder, "_xocc_Stencil_sdaccel_hw.dir",
+      "impl", "build", "system", "sdaccel_hw", "bitstream")
+  try:
+    log = open(
+        os.path.join(confFolder, "log.out"), "r").read()
+  except:
+    return "no_build"
   try:
     report = open(
         os.path.join(kernelFolder, "sdaccel_hw_ipi", "vivado.log")).read()
   except:
     return "no_build"
+  m = re.search("Detail Placement failed", log)
+  if m:
+    return "failed_placement"
+  m = re.search("Internal Data Exception", log)
+  if m:
+    return "crashed"
   m = re.search("auto frequency scaling failed", report)
   if m:
     return "failed_timing"
