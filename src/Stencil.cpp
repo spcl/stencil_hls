b5988e24c3b14e95068b43774e27a53eb52baffe
Johannes de Fine Licht <johannes.definelicht@inf.ethz.ch>
2017-03-20 12:51:18 +0100

    Plugged in stencil kernels

--- src/Stencil.cpp
+++ src/Stencil.cpp
@@ -4,10 +4,11 @@
 #include "Stencil.h"
 #include <hls_stream.h>
 #include <cassert>
-#ifndef BENCHMARK_SYNTHESIS
+#ifndef STENCIL_SYNTHESIS
 #include <thread>
 #endif
 #include <hlslib/Stream.h>
+#include <hlslib/Utility.h>
 
 void Read(Memory_t const *input, hlslib::Stream<Memory_t> &buffer) {
 ReadTime:
@@ -90,38 +91,242 @@ WidenTime:
   }
 }
 
-void Compute(hlslib::Stream<Kernel_t> &in, hlslib::Stream<Kernel_t> &out) {
-ComputeTime:
-  for (int t = 0; t < kTimeFolded; ++t) {
-  ComputeBlocks:
-    for (int b = 0; b < kBlocks; ++b) {
-    ComputeRows:
-      for (int r = 0; r < kRows; ++r) {
-      ComputeCols:
-        for (int c = 0; c < kBlockWidthKernel + 2 * kHaloKernel; ++c) {
-          #pragma HLS LOOP_FLATTEN
-          #pragma HLS PIPELINE
-          Kernel_t read;
-          if ((c >= kHaloKernel || b > 0) &&
-              (c < kBlockWidthKernel + kHaloKernel || b < kBlocks - 1)) {
-            read = hlslib::ReadBlocking(in);
-          } else {
-            read = Kernel_t(static_cast<Data_t>(0));
-          }
-          Kernel_t result;
-        SIMD:
-          for (int w = 0; w < kKernelWidth; ++w) {
-            #pragma HLS UNROLL
-            result[w] = read[w] + 1;
-          }
-          if (c >= kHaloKernel && c < kBlockWidthKernel + kHaloKernel) {
-            hlslib::WriteBlocking(out, result, 256);
-          }
-        }
+template <int stage>
+void Compute(hlslib::Stream<Kernel_t> &pipeIn,
+             hlslib::Stream<Kernel_t> &pipeOut) {
+
+  static constexpr int kInputWidth =
+      kBlockWidthKernel + 2 * hlslib::CeilDivide(kDepth - stage, kKernelWidth);
+
+  // Size of the halo in either side
+  static constexpr int kBoundaryWidth = (kInputWidth - kBlockWidthKernel) / 2;
+
+  // Begin and end indices of the inner block size (without any halos)
+  static constexpr int kInnerBegin = kBoundaryWidth;
+  static constexpr int kInnerEnd = kInputWidth - kBoundaryWidth;
+
+  // Only shrink the output if we hit the boundary of the data width
+  static constexpr bool kShrinkOutput =
+      (kKernelWidth + kDepth - stage - 1) % kKernelWidth == 0;
+
+  // The stencil radius is always smaller than the halo size, so we always
+  // shrink by one
+  static constexpr int kOutputBegin = kShrinkOutput ? 1 : 0;
+  static constexpr int kOutputEnd =
+      kShrinkOutput ? kInputWidth - 1 : kInputWidth;
+
+  // The typedef seems to break the high level synthesis tool when applying
+  // pragmas
+#ifndef STENCIL_SYNTHESIS 
+  hlslib::Stream<Kernel_t> northBuffer("northBuffer");
+  hlslib::Stream<Kernel_t> centerBuffer("centerBuffer");
+#else
+  hls::stream<Kernel_t> northBuffer("northBuffer");
+  #pragma HLS STREAM variable=northBuffer depth=kInputWidth
+  #pragma HLS RESOURCE variable=northBuffer core=FIFO_BRAM
+  hls::stream<Kernel_t> centerBuffer("centerBuffer");
+  #pragma HLS STREAM variable=centerBuffer depth=kInputWidth
+  #pragma HLS RESOURCE variable=centerBuffer core=FIFO_BRAM
+#endif
+
+  int t = 0;
+  int b = 0;
+  int r = 0;
+  int c = 0;
+
+  Data_t shiftWest(kBoundary);
+  Kernel_t shiftCenter(kBoundary);
+
+ComputeFlat:
+  for (long i = 0;
+       i < kTimeFolded * kBlocks * kRows * kInputWidth + kInputWidth - 1; ++i) {
+    #pragma HLS PIPELINE
+
+#ifdef STENCIL_KERNEL_DEBUG
+    std::stringstream debugStream;
+    debugStream << "Stage " << stage << ": (" << t << ", " << b << ", "
+                << r << ", " << c - kBoundaryWidth << "): ";
+    bool debugCond = stage == 0 && r == 0 && c - kBoundaryWidth == 0 && b == 0;
+#endif
+
+    const bool isSaturating = i < kInputWidth - 1; // Shift right by one 
+    const bool isDraining = i >= kTimeFolded * kBlocks * kRows * kInputWidth;
+    const bool inBounds = ((b > 0 || c >= kInnerBegin) &&
+                           (b < kBlocks - 1 || c < kInnerEnd));
+    const bool inBlock = c >= kInnerBegin && c < kInnerEnd;
+
+    if (isSaturating) {
+      Kernel_t read;
+      if (i >= kInnerBegin - 1) { // Shift right by one
+        read = hlslib::ReadBlocking(pipeIn);
+      } else {
+        read = Kernel_t(kBoundary);
+      }
+      hlslib::WriteOptimistic(centerBuffer, read, kInputWidth);
+#ifdef STENCIL_KERNEL_DEBUG
+      debugStream << "saturating " << read << "\n";
+      if (debugCond) {
+        std::cout << debugStream.str();
+      }
+#endif
+      continue;
+    }
+
+    Kernel_t read;
+    if (!isDraining) {
+      // If not on the last row, check if the column in this block is out
+      // of bounds. If on the last row, check if the column is out of
+      // bounds in the NEXT block. Written below in DNF form:
+      if ((b == 0 && c < kInnerBegin && r < kRows - 1) ||
+          (b == kBlocks - 1 && c < kInnerBegin && r == kRows - 1) ||
+          (b == kBlocks - 1 && c >= kInnerEnd && r < kRows - 1) ||
+          (b == kBlocks - 2 && c >= kInnerEnd && r == kRows - 1)) {
+
+        read = Kernel_t(kBoundary);
+
+      } else {
+
+        read = hlslib::ReadBlocking(pipeIn);
+
       }
     }
+
+    // Collect vertical values
+    const auto north =
+        (r > 0) ? hlslib::ReadOptimistic(northBuffer) : Kernel_t(kBoundary);
+    const auto south = (r < kRows - 1) ? read : Kernel_t(kBoundary);
+
+    // Use center value shifted forward to populate bulk of west and east
+    // vectors
+    Kernel_t west, east;
+    shiftCenter.ShiftTo<0, 1, kKernelWidth - 1>(west);
+    shiftCenter.ShiftTo<1, 0, kKernelWidth - 1>(east);
+
+    // Read the next center value to collect the last element of east
+    const auto nextCenter = hlslib::ReadOptimistic(centerBuffer);
+    east[kKernelWidth - 1] = nextCenter[0];
+
+    // We shifted the last element of west forward from last iteration
+    west[0] = shiftWest;
+
+    // Now update the line buffers
+    if (!isDraining) {
+      hlslib::WriteOptimistic(centerBuffer, read, kInputWidth);
+      if (r < kRows - 1) {
+        hlslib::WriteOptimistic(northBuffer, shiftCenter, kInputWidth);
+      }
+    }
+
+    // Values have been consumed, so shift all center registers left 
+    shiftWest = shiftCenter[kKernelWidth - 1];
+    shiftCenter = nextCenter;
+
+#ifdef STENCIL_KERNEL_DEBUG
+    debugStream << "N" << north << ", W" << west << ", E" << east << ", S"
+                << south;
+#endif
+
+    // Now we can perform the actual compute
+    Kernel_t result;
+  ComputeSIMD:
+    for (int w = 0; w < kKernelWidth; ++w) {
+      #pragma HLS UNROLL
+      const Data_t northVal = north[w];
+      const Data_t westVal = west[w];
+      const Data_t eastVal = east[w];
+      const Data_t southVal = south[w];
+      const Data_t factor = 0.25;
+      result[w] = factor * (northVal + westVal + eastVal + southVal);
+    }
+
+    // Only output values if the next unit needs them
+    if (c >= kOutputBegin && c < kOutputEnd && inBounds) {
+      Kernel_t write;
+      if (inBounds) {
+        write = result;
+      } else {
+        write = Kernel_t(kBoundary);
+      }
+      hlslib::WriteBlocking(pipeOut, write, kPipeDepth);
+#ifdef STENCIL_KERNEL_DEBUG
+      if (debugCond) {
+        debugStream << " -> " << write << "\n"; 
+      }
+#endif
+    } else {
+#ifdef STENCIL_KERNEL_DEBUG
+      if (debugCond) {
+        debugStream << " no output.\n";
+      }
+#endif
+    }
+
+#ifdef STENCIL_KERNEL_DEBUG
+    if (debugCond) {
+      std::cout << debugStream.str();
+    }
+#endif
+
+    // Index calculations
+    if (c == kInputWidth - 1) {
+      c = 0;
+      if (r == kRows - 1) {
+        r = 0;
+        if (b == kBlocks - 1) {
+          b = 0;
+          ++t;
+        } else {
+          ++b;
+        }
+      } else {
+        ++r;
+      }
+    } else {
+      ++c;
+    }
   }
+
 }
+
+#ifdef STENCIL_SYNTHESIS
+
+template <int stage>
+void UnrollCompute(hlslib::Stream<Kernel_t> &previous,
+                   hlslib::Stream<Kernel_t> &last) {
+  #pragma HLS INLINE
+  hls::stream<Kernel_t> next("pipe");
+  #pragma HLS STREAM variable=next depth=kPipeDepth
+  Compute<kDepth - stage>(previous, next);
+  UnrollCompute<stage - 1>(next, last);
+}
+
+template <>
+void UnrollCompute<1>(hlslib::Stream<Kernel_t> &previous,
+                      hlslib::Stream<Kernel_t> &last) {
+#pragma HLS INLINE
+  Compute<kDepth - 1>(previous, last);
+}
+
+#else
+
+template <int stage>
+void UnrollCompute(hlslib::Stream<Kernel_t> &previous,
+                   hlslib::Stream<Kernel_t> &last,
+                   std::vector<std::thread> &threads) {
+  static hlslib::Stream<Kernel_t> next("pipe");
+  threads.emplace_back(Compute<kDepth - stage>, std::ref(previous),
+                       std::ref(next));
+  UnrollCompute<stage - 1>(next, last, threads);
+}
+
+template <>
+void UnrollCompute<1>(hlslib::Stream<Kernel_t> &previous,
+                      hlslib::Stream<Kernel_t> &last,
+                      std::vector<std::thread> &threads) {
+  threads.emplace_back(Compute<kDepth - 1>, std::ref(previous), std::ref(last));
+}
+
+#endif
 
 void Narrow(hlslib::Stream<Kernel_t> &in, hlslib::Stream<Memory_t> &out) {
   Memory_t memoryBlock;
@@ -179,21 +384,20 @@ void Jacobi(Memory_t const *in, Memory_t *out) {
   #pragma HLS INTERFACE s_axilite port=out    bundle=control 
   #pragma HLS INTERFACE s_axilite port=return bundle=control 
   #pragma HLS DATAFLOW
-#ifndef BENCHMARK_SYNTHESIS
+#ifndef STENCIL_SYNTHESIS
+  std::vector<std::thread> threads;
   hlslib::Stream<Memory_t> readBuffer("readBuffer");
   hlslib::Stream<Kernel_t> kernelPipeIn("kernelPipeIn");
   hlslib::Stream<Kernel_t> kernelPipeOut("kernelPipeOut");
   hlslib::Stream<Memory_t> writeBuffer("writeBuffer");
-  std::thread read(Read, in, std::ref(readBuffer));
-  std::thread widen(Widen, std::ref(readBuffer), std::ref(kernelPipeIn));
-  std::thread compute(Compute, std::ref(kernelPipeIn), std::ref(kernelPipeOut));
-  std::thread narrow(Narrow, std::ref(kernelPipeOut), std::ref(writeBuffer));
-  std::thread write(Write, std::ref(writeBuffer), out);
-  read.join();
-  widen.join();
-  compute.join();
-  narrow.join();
-  write.join();
+  threads.emplace_back(Read, in, std::ref(readBuffer));
+  threads.emplace_back(Widen, std::ref(readBuffer), std::ref(kernelPipeIn));
+  UnrollCompute<kDepth>(kernelPipeIn, kernelPipeOut, threads);
+  threads.emplace_back(Narrow, std::ref(kernelPipeOut), std::ref(writeBuffer));
+  threads.emplace_back(Write, std::ref(writeBuffer), out);
+  for (auto &t : threads) {
+    t.join();
+  }
 #else
   hls::stream<Memory_t> readBuffer("readBuffer");
   #pragma HLS STREAM variable=readBuffer depth=kMemoryBufferDepth
@@ -205,7 +409,7 @@ void Jacobi(Memory_t const *in, Memory_t *out) {
   #pragma HLS STREAM variable=writeBuffer depth=kMemoryBufferDepth
   Read(in, readBuffer);
   Widen(readBuffer, kernelPipeIn);
-  Compute(kernelPipeIn, kernelPipeOut);
+  UnrollCompute<kDepth>(kernelPipeIn, kernelPipeOut);
   Narrow(kernelPipeOut, writeBuffer);
   Write(writeBuffer, out);
 #endif
--- src/Testbench.cpp
+++ src/Testbench.cpp
@@ -2,38 +2,59 @@
 #include <iostream>
 #include <vector>
 
-int main() {
-  std::vector<Memory_t> memory(2 * kTotalElementsMemory);
-  for (int i = 0; i < kTotalElementsMemory; ++i) {
-    memory[i] = Memory_t(Kernel_t(static_cast<Data_t>(i)));
+std::vector<Data_t> Reference(std::vector<Data_t> const &input) {
+  std::vector<Data_t> domain(input);
+  std::vector<Data_t> buffer(input);
+  for (int t = 0; t < kTimeTotal; ++t) {
+    for (int r = 0; r < kRows; ++r) {
+      for (int c = 0; c < kCols; ++c) {
+        const Data_t west =
+            (c == 0) ? Data_t(1) : domain[r * kCols + c - 1];
+        const Data_t east =
+            (c == kCols - 1) ? Data_t(1) : domain[r * kCols + c + 1];
+        const Data_t north =
+            (r == 0) ? Data_t(1) : domain[(r - 1) * kCols + c];
+        const Data_t south =
+            (r == kRows - 1) ? Data_t(1) : domain[(r + 1) * kCols + c];
+        buffer[r * kCols + c] =
+            Data_t(0.25) * (north + west + east + south);
+      }
+    }
+    domain.swap(buffer);
   }
+  return domain;
+}
+
+int main() {
+  const auto reference = Reference(std::vector<Data_t>(kRows * kCols, 0));
+  std::vector<Memory_t> memory(2 * kTotalElementsMemory,
+                               Kernel_t(Data_t(static_cast<Data_t>(0))));
   Jacobi(memory.data(), memory.data());
   // int correct = 0;
   // int mismatches = 0;
-  for (int offset = 0; offset < 2 * kTotalElementsMemory;
-       offset += kTotalElementsMemory) {
-    for (int r = 0; r < kRows; ++r) {
-      for (int c = 0; c < kBlockWidthMemory * kBlocks; ++c) {
-        const int index = offset + r * kBlockWidthMemory * kBlocks + c;
-        const Data_t expected =
-            r * kBlockWidthMemory * kBlocks + c +
-            ((offset == 0) ? kTimeFolded : (kTimeFolded - 1));
-        for (int k = 0; k < kKernelPerMemory; ++k) {
-          const Kernel_t elem = memory[index][k];
-          for (int w = 0; w < kKernelWidth; ++w) {
-            if (elem[w] != expected) {
-              std::cerr << "Mismatch at (" << r << ", "
-                        << c * kMemoryWidth + k * kKernelWidth + w
-                        << "): " << elem[w] << " (should be " << expected
-                        << ")" << std::endl;
-              // ++mismatches;
-              return 1;
-            } else {
-              // std::cout << "Correct at (" << r << ", "
-              //           << c * kMemoryWidth + k * kKernelWidth + w
-              //           << "): " << elem[w] << "\n";
-              // ++correct;
-            }
+  const int offset = (kTimeFolded % 2 == 0) ? 0 : kTotalElementsMemory;
+  for (int r = 0; r < kRows; ++r) {
+    for (int c = 0; c < kBlockWidthMemory * kBlocks; ++c) {
+      const int index = r * kBlockWidthMemory * kBlocks + c;
+      for (int k = 0; k < kKernelPerMemory; ++k) {
+        const Kernel_t elem = memory[offset + index][k];
+        for (int w = 0; w < kKernelWidth; ++w) {
+          const auto expected =
+              reference[kMemoryWidth * index + kKernelWidth * k + w];
+          const auto actual = elem[w];
+          const auto diff = std::fabs(expected - actual); 
+          if (diff > 1e-4) {
+            std::cerr << "Mismatch at (" << r << ", "
+                      << c * kMemoryWidth + k * kKernelWidth + w
+                      << "): " << actual << " (should be " << expected << ")"
+                      << std::endl;
+            // ++mismatches;
+            return 1;
+          } else {
+            // std::cout << "Correct at (" << r << ", "
+            //           << c * kMemoryWidth + k * kKernelWidth + w
+            //           << "): " << elem[w] << "\n";
+            // ++correct;
           }
         }
       }
