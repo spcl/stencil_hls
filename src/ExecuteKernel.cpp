22fdebb8e48b15234446c2ab2dca99a20a0e0dc5
Johannes de Fine Licht <johannes.definelicht@inf.ethz.ch>
2017-04-03 15:46:46 +0200

    Implemented kernel execution for 2 DIMMs and verified in hardware (with only one physical dimm)

--- src/ExecuteKernel.cpp
+++ src/ExecuteKernel.cpp
@@ -26,89 +26,246 @@ int main(int argc, char **argv) {
     }
   }
 
-  std::vector<Memory_t> host;
+  if (kDimms == 1) {
 
-  try {
+    std::vector<Memory_t> host;
 
-    hlslib::ocl::Context context("Xilinx", kDeviceDsaString);
+    try {
 
-    auto device = context.MakeBuffer<Memory_t, hlslib::ocl::Access::readWrite>(
-        hlslib::ocl::MemoryBank::bank0, 2 * kTotalElementsMemory);
+      std::cout << "Creating OpenCL context..." << std::flush;
+      hlslib::ocl::Context context("Xilinx", kDeviceDsaString);
+      std::cout << " Done." << std::endl;
 
-    if (verify) {
-      host = std::vector<Memory_t>(2 * kTotalElementsMemory,
-                                   Memory_t(Kernel_t(static_cast<Data_t>(0))));
-      device.CopyToDevice(host.cbegin());
+      std::cout << "Allocating device memory..." << std::flush;
+      auto device = context.MakeBuffer<Memory_t, hlslib::ocl::Access::readWrite>(
+          hlslib::ocl::MemoryBank::bank0, 2 * kTotalElementsMemory);
+      std::cout << " Done." << std::endl;
+
+      if (verify) {
+        std::cout << "Initializing memory..." << std::flush;
+        host = std::vector<Memory_t>(2 * kTotalElementsMemory,
+                                     Memory_t(Kernel_t(static_cast<Data_t>(0))));
+        device.CopyToDevice(host.cbegin());
+        std::cout << " Done." << std::endl;
+      }
+
+      std::cout << "Creating kernel..." << std::flush;
+      auto kernel =
+          context.MakeKernelFromBinary(kKernelString, "Jacobi", device, device);
+      std::cout << " Done." << std::endl;
+
+      const auto readSize =
+          static_cast<float>(kTimeFolded) * kTotalInputMemory * sizeof(Memory_t);
+      const auto writeSize = static_cast<float>(kTimeFolded) *
+                             kTotalElementsMemory * sizeof(Memory_t);
+      const auto transferred = readSize + writeSize;
+
+      std::cout << "Executing kernel..." << std::flush;
+      auto begin = std::chrono::high_resolution_clock::now();
+      kernel.ExecuteTask();
+      auto end = std::chrono::high_resolution_clock::now();
+      double elapsed =
+          1e-9 *
+          std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin)
+              .count();
+      std::cout << " Done.\nMoved " << 1e-9 * transferred << " GB in " << elapsed
+                << " seconds, bandwidth " << (1e-9 * transferred / elapsed)
+                << " GB/s\nEvaluated " << kTimeTotal * kRows * kCols
+                << " cells in " << elapsed << " seconds, performance "
+                << 4e-9 * (kTimeTotal * kRows * kCols) / elapsed << " GOp/s"
+                << std::endl;
+      if (verify) {
+        std::cout << "Copying back memory..." << std::flush;
+        device.CopyToHost(host.begin());
+        std::cout << " Done." << std::endl;
+      }
+
+    } catch (std::runtime_error const &err) {
+      std::cerr << "Execution failed with error: \"" << err.what() << "\"."
+                << std::endl;
+      return 1;
     }
 
-    auto kernel =
-        context.MakeKernelFromBinary(kKernelString, "Jacobi", device, device);
-
-    const auto readSize =
-        static_cast<float>(kTimeFolded) * kTotalInputMemory * sizeof(Memory_t);
-    const auto writeSize = static_cast<float>(kTimeFolded) *
-                           kTotalElementsMemory * sizeof(Memory_t);
-    const auto transferred = readSize + writeSize;
-
-    std::cout << "Executing kernel..." << std::flush;
-    auto begin = std::chrono::high_resolution_clock::now();
-    kernel.ExecuteTask();
-    auto end = std::chrono::high_resolution_clock::now();
-    double elapsed =
-        1e-9 *
-        std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin)
-            .count();
-    std::cout << " Done.\nMoved " << 1e-9 * transferred << " GB in " << elapsed
-              << " seconds, bandwidth " << (1e-9 * transferred / elapsed)
-              << " GB/s\nEvaluated " << kTimeTotal * kRows * kCols
-              << " cells in " << elapsed << " seconds, performance "
-              << 4e-9 * (kTimeTotal * kRows * kCols) / elapsed << " GOp/s"
-              << std::endl;
+    // Verification
     if (verify) {
-      device.CopyToHost(host.begin());
-    }
-
-  } catch (std::runtime_error const &err) {
-    std::cerr << "Execution failed with error: \"" << err.what() << "\"."
-              << std::endl;
-    return 1;
-  }
-
-  // Verification
-  if (verify) {
-    // int correct = 0;
-    // int mismatches = 0;
-    const auto reference = Reference(std::vector<Data_t>(kRows * kCols, 0));
-    const int offset = (kTimeFolded % 2 == 0) ? 0 : kTotalElementsMemory;
-    for (int r = 0; r < kRows; ++r) {
-      for (int c = 0; c < kBlockWidthMemory * kBlocks; ++c) {
-        const int index = r * kBlockWidthMemory * kBlocks + c;
-        for (int k = 0; k < kKernelPerMemory; ++k) {
-          const Kernel_t elem = host[offset + index][k];
-          for (int w = 0; w < kKernelWidth; ++w) {
-            const auto expected =
-                reference[kMemoryWidth * index + kKernelWidth * k + w];
-            const auto actual = elem[w];
-            const auto diff = std::fabs(expected - actual);
-            if (diff > 1e-4) {
-              std::cerr << "Mismatch at (" << r << ", "
-                        << c * kMemoryWidth + k * kKernelWidth + w
-                        << "): " << actual << " (should be " << expected << ")"
-                        << std::endl;
-              // ++mismatches;
-              return 1;
-            } else {
-              // std::cout << "Correct at (" << r << ", "
-              //           << c * kMemoryWidth + k * kKernelWidth + w
-              //           << "): " << elem[w] << "\n";
-              // ++correct;
+      int correct = 0;
+      int mismatches = 0;
+      std::cout << "Running reference implementation..." << std::flush;
+      const auto reference = Reference(std::vector<Data_t>(kRows * kCols, 0));
+      std::cout << " Done." << std::endl;
+      std::cout << "Verifying result..." << std::flush;
+      const int offset = (kTimeFolded % 2 == 0) ? 0 : kTotalElementsMemory;
+      for (int r = 0; r < kRows; ++r) {
+        for (int c = 0; c < kBlockWidthMemory * kBlocks; ++c) {
+          const int index = r * kBlockWidthMemory * kBlocks + c;
+          for (int k = 0; k < kKernelPerMemory; ++k) {
+            const Kernel_t elem = host[offset + index][k];
+            for (int w = 0; w < kKernelWidth; ++w) {
+              const auto expected =
+                  reference[kMemoryWidth * index + kKernelWidth * k + w];
+              const auto actual = elem[w];
+              const auto diff = std::fabs(expected - actual);
+              if (diff > 1e-4) {
+                if (mismatches == 0) {
+                  std::cerr << "Mismatch at (" << r << ", "
+                            << c * kMemoryWidth + k * kKernelWidth + w
+                            << "): " << actual << " (should be " << expected << ")"
+                            << std::endl;
+                }
+                ++mismatches;
+              } else {
+                // std::cout << "Correct at (" << r << ", "
+                //           << c * kMemoryWidth + k * kKernelWidth + w
+                //           << "): " << elem[w] << "\n";
+                ++correct;
+              }
             }
           }
         }
       }
+      std::cout << " Done." << std::endl;
+      std::cout << "Correct: " << correct << "\nMismatches: " << mismatches
+                << std::endl;
+      if (mismatches == 0) {
+        std::cout << "Verification successful." << std::endl;
+      } else {
+        std::cerr << "Verification failed." << std::endl;
+        return 1;
+      }
     }
-    // std::cout << "Correct: " << correct << "\nMismatches: " << mismatches
-    //           << std::endl;
+
+  } else if (kDimms == 2) {
+
+    std::vector<Memory_t> hostSplit0;
+    std::vector<Memory_t> hostSplit1;
+
+    try {
+
+      std::cout << "Creating OpenCL context..." << std::flush;
+      hlslib::ocl::Context context("Xilinx", kDeviceDsaString);
+      std::cout << " Done." << std::endl;
+
+      std::cout << "Allocating device memory..." << std::flush;
+      auto device0 =
+          context.MakeBuffer<Memory_t, hlslib::ocl::Access::readWrite>(
+              hlslib::ocl::MemoryBank::bank0, kTotalElementsMemory);
+      auto device1 =
+          context.MakeBuffer<Memory_t, hlslib::ocl::Access::readWrite>(
+              hlslib::ocl::MemoryBank::bank0, kTotalElementsMemory);
+      std::cout << " Done." << std::endl;
+
+      if (verify) {
+        std::cout << "Initializing memory..." << std::flush;
+        hostSplit0 = std::vector<Memory_t>(
+            kTotalElementsMemory, Memory_t(Kernel_t(static_cast<Data_t>(0))));
+        hostSplit1 = std::vector<Memory_t>(
+            kTotalElementsMemory, Memory_t(Kernel_t(static_cast<Data_t>(0))));
+        device0.CopyToDevice(hostSplit0.cbegin());
+        device1.CopyToDevice(hostSplit1.cbegin());
+        std::cout << " Done." << std::endl;
+      }
+
+      std::cout << "Creating kernel..." << std::flush;
+      auto kernel = context.MakeKernelFromBinary(
+          kKernelString, "JacobiTwoDimms", device0, device0, device1, device1);
+      std::cout << " Done." << std::endl;
+
+      const auto readSize =
+          static_cast<float>(kTimeFolded) * kTotalInputMemory * sizeof(Memory_t);
+      const auto writeSize = static_cast<float>(kTimeFolded) *
+                             kTotalElementsMemory * sizeof(Memory_t);
+      const auto transferred = readSize + writeSize;
+
+      std::cout << "Executing kernel..." << std::flush;
+      auto begin = std::chrono::high_resolution_clock::now();
+      kernel.ExecuteTask();
+      auto end = std::chrono::high_resolution_clock::now();
+      double elapsed =
+          1e-9 *
+          std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin)
+              .count();
+      std::cout << " Done.\nMoved " << 1e-9 * transferred << " GB in " << elapsed
+                << " seconds, bandwidth " << (1e-9 * transferred / elapsed)
+                << " GB/s\nEvaluated " << kTimeTotal * kRows * kCols
+                << " cells in " << elapsed << " seconds, performance "
+                << 4e-9 * (kTimeTotal * kRows * kCols) / elapsed << " GOp/s"
+                << std::endl;
+      if (verify) {
+        std::cout << "Copying back memory..." << std::flush;
+        device0.CopyToHost(hostSplit0.begin());
+        device1.CopyToHost(hostSplit1.begin());
+        std::cout << " Done." << std::endl;
+      }
+
+    } catch (std::runtime_error const &err) {
+      std::cerr << "Execution failed with error: \"" << err.what() << "\"."
+                << std::endl;
+      return 1;
+    }
+
+    // Verification
+    if (verify) {
+      std::cout << "Reassembling memory..." << std::flush;
+      std::vector<Memory_t> host(kTotalElementsMemory);
+      for (int rIn = 0, rOut = 0; rOut < kRows; ++rIn, rOut += 2) {
+        static constexpr auto kMemoryCols = kCols / kMemoryWidth;
+        const auto iStart = rIn * kMemoryCols;
+        std::copy(hostSplit0.begin() + iStart,
+                  hostSplit0.begin() + iStart + kMemoryCols,
+                  host.begin() + rOut * kMemoryCols);
+        std::copy(hostSplit1.begin() + iStart,
+                  hostSplit1.begin() + iStart + kMemoryCols,
+                  host.begin() + (rOut + 1) * kMemoryCols);
+      }
+      std::cout << " Done." << std::endl;
+      int correct = 0;
+      int mismatches = 0;
+      std::cout << "Running reference implementation..." << std::flush;
+      const auto reference = Reference(std::vector<Data_t>(kRows * kCols, 0));
+      std::cout << " Done." << std::endl;
+      std::cout << "Verifying result..." << std::flush;
+      const int offset = (kTimeFolded % 2 == 0) ? 0 : kTotalElementsMemory;
+      for (int r = 0; r < kRows; ++r) {
+        for (int c = 0; c < kBlockWidthMemory * kBlocks; ++c) {
+          const int index = r * kBlockWidthMemory * kBlocks + c;
+          for (int k = 0; k < kKernelPerMemory; ++k) {
+            const Kernel_t elem = host[offset + index][k];
+            for (int w = 0; w < kKernelWidth; ++w) {
+              const auto expected =
+                  reference[kMemoryWidth * index + kKernelWidth * k + w];
+              const auto actual = elem[w];
+              const auto diff = std::fabs(expected - actual);
+              if (diff > 1e-4) {
+                if (mismatches == 0) {
+                  std::cerr << "Mismatch at (" << r << ", "
+                            << c * kMemoryWidth + k * kKernelWidth + w
+                            << "): " << actual << " (should be " << expected << ")"
+                            << std::endl;
+                }
+                ++mismatches;
+              } else {
+                // std::cout << "Correct at (" << r << ", "
+                //           << c * kMemoryWidth + k * kKernelWidth + w
+                //           << "): " << elem[w] << "\n";
+                ++correct;
+              }
+            }
+          }
+        }
+      }
+      std::cout << " Done." << std::endl;
+      std::cout << "Correct: " << correct << "\nMismatches: " << mismatches
+                << std::endl;
+      if (mismatches == 0) {
+        std::cout << "Verification successful." << std::endl;
+      } else {
+        std::cerr << "Verification failed." << std::endl;
+        return 1;
+      }
+    }
+
+  } else {
+    throw std::runtime_error("Unrecognized number of DIMMs.");
   }
 
   return 0;
